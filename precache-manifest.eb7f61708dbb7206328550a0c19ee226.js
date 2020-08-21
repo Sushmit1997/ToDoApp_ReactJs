fb41a28b37ca5c6ea9ea53d9e3695960330e8fd2
Sushmit Rajaure <39255317+Sushmit1997@users.noreply.github.com>
2020-08-21 21:41:47 +0545

    Updates

---
+++ precache-manifest.eb7f61708dbb7206328550a0c19ee226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe3b455ca4d27e9269b1c046e0603dc",
+    "url": "/ToDoApp_ReactJs/index.html"
+  },
+  {
+    "revision": "4c94574662598599538a",
+    "url": "/ToDoApp_ReactJs/static/css/main.25db3c3d.chunk.css"
+  },
+  {
+    "revision": "df9edbb8e1d26167cdc3",
+    "url": "/ToDoApp_ReactJs/static/js/2.fac61e32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp_ReactJs/static/js/2.fac61e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c94574662598599538a",
+    "url": "/ToDoApp_ReactJs/static/js/main.024efce1.chunk.js"
+  },
+  {
+    "revision": "a680f1e881c6b148fc69",
+    "url": "/ToDoApp_ReactJs/static/js/runtime-main.e29df598.js"
+  }
+]);
